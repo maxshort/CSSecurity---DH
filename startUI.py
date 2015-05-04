fa215a122f58bcc81bcc534de9c86a014f890bfa
maxshort <maxshort@users.noreply.github.com>
2015-05-04 09:50:42 -0500

    Light documentation

--- startUI.py
+++ startUI.py
@@ -7,16 +7,36 @@ import queue
 import sys
 
 class Application(tkinter.Frame):
+    """Base of the application, can contain a setup window or a chat window"""
     def __init__(self,master=None):
         tkinter.Frame.__init__(self,master)
         #self.chat = Chat(self)
         #self.chat.grid(row = 0, column = 0)
-        self.setup = Setup(self, ("127.0.0.1",25124))
+        self.setup = Setup(self, ("127.0.0.1",25123))
         self.setup.grid(row = 0, column =0)
 
     
-class Chat(tkinter.Frame): 
+class Chat(tkinter.Frame):
+    """Chat interface. Has a box for chat history and a box/button to send new messages
+
+       Attributes:
+           soc (socket): a socket to communicate with other party
+           encrypter(encryption class, optional): must have encrypt and decrypt methods (see encrypt.Vigenere or encrypt.Caesar) 
+           master(tkinter.Frame): the frame within which to embed this frame
+           messageQueue(queue): Queue where messages to be put on local ui are
+              queued. Does not do networking. Messages should be formatted and
+              decrypted before being placed in the message queue as the strings
+              coming out of it are simply appended.
+    """
     def __init__(self, soc, encrypter = None, master=None):
+        """Initializes application window
+
+        Args:
+            soc (socket): a socket to communicate with other party
+            encrypter(encryption class, optional): must have encrypt and decrypt methods (see encrypt.Vigenere or encrypt.Caesar) 
+            master(tkinter.Frame, optional): the frame within which to embed this frame
+
+        """
         tkinter.Frame.__init__(self,master)
         self.createWidgets()
         self.soc = soc
@@ -30,6 +50,12 @@ class Chat(tkinter.Frame):
         self.postMessages()
 
     def createWidgets(self):
+        """Initializes UI.
+
+        Creates a box for message history, a box for new messages, a button
+        for new messages and associated labels.
+
+        """
         self.text = tkinter.Text(self)
         self.text.config(state=tkinter.DISABLED)
         self.text.grid(row =0, column = 0, rowspan = 2, columnspan=6)
@@ -48,6 +74,7 @@ class Chat(tkinter.Frame):
 
     #posts own and sends over network    
     def sendMessage(self):
+        """Puts a message into the message Queue and also sends it over the network"""
         messageToSend = self.entry.get()
         if self.encrypter is not None:
             self.soc.send(bytearray(self.encrypter.encrypt(messageToSend), "utf-8"))
@@ -57,6 +84,12 @@ class Chat(tkinter.Frame):
     #Have  to run this on main thread.
     #Messages should be in plaintext by this point.
     def postMessages(self):
+        """Reads from the message queue and posts the message
+
+        Any formatting (e.g. names, new lines) should be done
+        before entering messages into the messageQueue. Also,
+        they should be decrypted at this point.
+        """
         while True:
             try:
                 self.update()
@@ -72,11 +105,19 @@ class Chat(tkinter.Frame):
             
     
     def keyPress(self, event):
+        """Used to monitor for an enter press in the new message box.
+
+        In the event of an enter press, it calls self.sendMessage()
+        """
         c = event.char
         if c == "\r":
             self.sendMessage()
 
     def listenForMessages(self):
+        """Listens from messages from other party over socket.
+           When message is received, decrypts (if necessary), formats it and
+           puts into self.messageQueue.
+        """
         while True:
             messageFromOther = self.soc.recv(4096)
             messageFromOther = str(messageFromOther,"utf-8")
@@ -86,9 +127,23 @@ class Chat(tkinter.Frame):
 
             
 class Setup(tkinter.Frame):
+    """User interface for creating paramters for networked DH Vigenere key exchange
 
+    Attributes:
+        parent(Application, optional): usually the same as master. Frame within in which the chat window will be embeded.
+        socketParams(tuple, optional): String address, int port
+        master(Tkinter.Frame, optional): The Frame within which this is embedded.
+    """
     #socketParams is a tuple of (addr, port) 
     def __init__(self, parent = None, socketParams=None, master=None):
+        """Creates a Setup frame.
+
+        Args:
+            parent(Application, optional): usually the same as master. Frame within in which the chat window will be embeded.
+            socketParams(tuple, optional): String address, int port
+            master(Tkinter.Frame, optional): The Frame within which this is embedded.
+        
+        """
         tkinter.Frame.__init__(self,master)
         self.createWidgets()
         self.parent = parent
@@ -99,6 +154,7 @@ class Setup(tkinter.Frame):
         self.listenerThread.start()
         
     def connectClicked(self):
+        """Exchanges key with other party, changes UI to chat"""
         if self.parent is not None:
             
             soc = socket.socket()
@@ -123,6 +179,7 @@ class Setup(tkinter.Frame):
 
 
     def createWidgets(self):
+        """Lays out setup UI"""
         self.title = tkinter.Label(self, text = "New Connection")
         self.title.grid(row = 0,column=0, columnspan=2)
 
@@ -140,6 +197,10 @@ class Setup(tkinter.Frame):
         self.connectButton.grid(row=3, column=1, sticky = tkinter.E)
 
     def listenForConnections(self):
+        """Listens for connections from another party.
+
+           Upon connection exchanges keys and sets up chat window.
+        """
         print("listening for connections")
         if self.socketParams is not None:
             try:
@@ -164,18 +225,30 @@ class Setup(tkinter.Frame):
                 raise
 
     def keyPress(self, event):
+        """Key press listener for address textbox.
+
+           If enter is clicked it calls self.connectClicked.
+        """
         c = event.char
         if c == "\r":
             self.connectClicked()
             
 class EncryptionParams(tkinter.Frame):
+    """Frame that holds a form for specifying parameters of DH/Vig key exchange
 
+    Attributes:
+        nBox(Entry): Value of n
+        gBox(Entry): Value of g
+        secretBox(Entry): Value of initial secret key
+    """
     def __init__(self, master=None):
+        """Creates EncryptionParams UI"""
         tkinter.Frame.__init__(self,master)
         self.config(bd=3,relief=tkinter.RIDGE)
         self.createWidgets()
         
     def createWidgets(self):
+        """Lays out UI"""
         self.title = tkinter.Label(self, text = "Encryption Parameters")
         self.title.grid(row=0, columnspan =2,sticky = tkinter.E+tkinter.W)
 
